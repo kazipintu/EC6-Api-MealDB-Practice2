08a82f4f49887c9ac34359a3823806c0891724d2
kazipintu <kazipintu@gmail.com>
2024-07-08 19:57:22 +0530

    EC6/Api/MealDB/Practice2

---
+++ js/index.js
@@ -0,0 +1,62 @@
+//console.log('connected');
+const searchFood = () => {
+  const searchInput = document.getElementById("search-input");
+  const serachInputValue = searchInput.value;
+  //console.log(serachInputValue);
+  fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${serachInputValue}`)
+    .then(response => response.json())
+    .then(data => displayMeals(data.meals))
+}
+
+const displayMeals = (foods) => {
+  //console.log(foods);
+  const displayFood = document.getElementById('display-meals');
+
+  foods.forEach(food => {
+    //console.log(food);
+    const displayDiv = document.createElement('div');
+    displayDiv.classList.add("col")
+    displayDiv.innerHTML = `
+      <div onclick="loadSingleMeal(${food.idMeal})" class="card">
+        <img src="${food.strMealThumb}" class="..." alt="...">
+        <div class="card-body">
+          <h5 class="card-title">${food.strMeal}</h5>
+          <p class="card-text">Recipe: ${food.strInstructions.slice(0, 200)}....</p>
+          <button type="button" class="btn btn-outline-primary w-100">Show Details</button>
+        </div>
+      </div>
+
+`
+    displayFood.appendChild(displayDiv)
+  })
+}
+
+const loadSingleMeal = (idRecipe) => {
+  //console.log(idRecipe);
+  const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${idRecipe}`
+  //console.log(url);
+  fetch(url)
+    .then(response => response.json())
+    .then(data => displaySingleMeal(data.meals[0]))
+}
+
+const displaySingleMeal = (recipe) => {
+  //console.log(recipe);
+  const displayMealDiv = document.getElementById('single-meal');
+  const displayRecipeDiv = document.createElement('div');
+  displayRecipeDiv.classList.add("col")
+
+  displayRecipeDiv.innerHTML = `
+    <div onclick="loadSingleMeal(${recipe.idMeal})" class="card">
+      <img src="${recipe.strMealThumb}" class="..." alt="...">
+      <div class="card-body">
+        <h5 class="card-title">${recipe.strMeal}</h5>
+        <p class="card-text">Recipe: ${recipe.strInstructions.slice(0, 400)}....</p>
+        <button type="button" class="btn btn-outline-primary w-100">Show Details</button>
+      </div>
+    </div>
+
+`
+displayMealDiv.appendChild(displayRecipeDiv)
+
+}
